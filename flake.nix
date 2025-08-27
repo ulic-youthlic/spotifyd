bbb366828821eda146d4b0537c7be16b108e2377
ulic-youthlic <ulic.youthlic@gmail.com>
2025-08-27 19:57:14 +0800

    Include nix build system

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rust-bin.stable.latest.default
+            rust-analyzer
+            pkg-config
+            openssl.dev
+            dbus.dev
+            alsa-lib.dev
+            pulseaudio.dev
+            libjack2
+          ];
+        };
+      }
+    );
+}
